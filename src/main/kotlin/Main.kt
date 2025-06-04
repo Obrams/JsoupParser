8db95dd8d4413f0c3819bde63f3384dab7d3a3ed
nikita <nickitarull@gmail.com>
2025-06-05 02:53:02 +0300

    KS-5-8 реализация задачи на парсинг

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,18 @@
 package org.example
 
-fun main() {
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import org.jsoup.nodes.Element
+import org.jsoup.select.Elements
 
+fun main() {
+    val doc: Document = Jsoup.connect(
+        "https://mybook.ru/author/duglas-adams/avtostopom-po-galaktike-restoran-u-konca-vselennoj/citations/?page=1"
+    )
+        .get()
+    val quotes: Elements = doc.select("div.sc-2aegk7-2.bzpNIu")
+
+    for (quote: Element in quotes) {
+        println("Цитата: ${quote.text()}\n")
+    }
 }
